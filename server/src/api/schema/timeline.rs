7f9e98efbdb19404533d12fa2f527e8544e8b995
Thomas Nibler <dev@tnibler.de>
2023-12-01 13:56:40 +0100

    note: smth about paging for later

--- server/src/api/schema/timeline.rs
+++ server/src/api/schema/timeline.rs
@@ -35,6 +35,25 @@ pub enum TimelineGroupType {
     },
 }
 
+// Paging API
+//
+// problem: timeline is not strictly chronological, since groups are displayed together even if
+// they're not necessarily contiguous.
+// that means "continue from this last timestamp" is not a valid strategy for paging the timeline.
+// solution: there are assets in the timeline where everything above has a strictly newer
+// timestamp, which can be used as "synchronization points".
+// The client could pass the last synchronization point date, and the last assets it has actually
+// displayed. The server can then use the synch point to compute the rest of the timeline,
+// and trim off the beginning until it reaches assets the clients doesn't have yet.
+//
+// An asset A is a synch point if the last asset of every group displayed above it has a date newer than A.
+//
+// so to start timeline at instant T with last displayed id I,
+// check if I is in a group (only one)
+// if yes get groupa, serve assets from that
+// when done, first asset older than group display date
+// => last date T not even needed?
+
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TimelineRequest {

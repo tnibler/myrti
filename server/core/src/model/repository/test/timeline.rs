4e36fa857747f31677c7cec63e4a93e8c85cdb94
Thomas Nibler <dev@tnibler.de>
2023-12-12 12:16:19 +0100

    add test todo

---
+++ server/core/src/model/repository/test/timeline.rs
@@ -0,0 +1,14 @@
+#[test]
+fn prop_test_timeline() {
+    // generate assets
+    // pick random distinct subsets
+    // create group albums from subsets, assigning random display_dates
+    // query timeline in different chunk sizes
+    //
+    // invariants in incresing order of complexity:
+    //  - no duplicate assets between chunks
+    //  - date ordering
+    //  - all assets contained in chunks
+    //  - chunks are correct
+    assert!(false);
+}

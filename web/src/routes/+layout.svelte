06bee6af3bd58edc85e038718cfb7a2284d74db8
Thomas Nibler <dev@tnibler.de>
2024-02-22 16:57:47 +0100

    web: basic sidebar

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Sidebar from './Sidebar.svelte';
+</script>
+
+<div id="page">
+	<Sidebar />
+	<div id="content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	#page {
+		overflow-y: hidden;
+		display: flex;
+		height: 100vh;
+	}
+
+	#content {
+		height: 100vh;
+		flex-grow: 1;
+	}
+	@media screen and (max-width: 600px) {
+		#sidebar {
+			display: none;
+		}
+	}
+</style>
---
+++ web/src/routes/Sidebar.svelte
@@ -0,0 +1,14 @@
+<div id="sidebar">
+	<ul>
+		<li>look a menu bar</li>
+		<li>menu item</li>
+	</ul>
+</div>
+
+<style>
+	#sidebar {
+		height: 100vh;
+		background-color: #ccc;
+		width: 200px;
+	}
+</style>

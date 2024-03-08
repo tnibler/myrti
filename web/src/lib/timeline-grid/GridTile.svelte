312059c95e45f4015655ec6abb8f111a7284de4f
Thomas Nibler <dev@tnibler.de>
2024-03-08 12:42:53 +0100

    web: styling for grid thumbnails

--- web/src/lib/timeline-grid/GridTile.svelte
+++ web/src/lib/timeline-grid/GridTile.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import type { Asset } from '$lib/apitypes';
 	import type { TimelineGridStore } from '$lib/store/timeline.svelte';
-	import { mdiCheckCircle, mdiCheckCircleOutline } from '@mdi/js';
+	import {
+		mdiCheckCircle,
+		mdiCheckCircleOutline,
+		mdiCheckboxMarkedCircle,
+		mdiCircleOutline
+	} from '@mdi/js';
 	import type { TileBox } from './GridSegment.svelte';
+	import { fade } from 'svelte/transition';
 
 	type GridTileProps = {
 		assetIndex: number;
@@ -21,6 +27,8 @@
 
 <a
 	href="#"
+	class="absolute group"
+	style="width: {box.width}px; height: {box.height}px; top: {box.top}px; left: {box.left}px;"
 	onclick={(e) => {
 		e.preventDefault();
 		if (inSelectionMode) {
@@ -36,35 +44,43 @@
 		isMouseOver = false;
 	}}
 >
-	<div>
+	<div class="h-full h-full bg-blue-100">
 		<!-- svelte-ignore a11y-missing-attribute -->
 		<img
 			bind:this={imgEl}
 			src="/api/asset/thumbnail/{asset.id}/large/avif"
-			class="absolute bg-black"
-			style="width: {box.width}px; height: {box.height}px; top: {box.top}px; left: {box.left}px; z-index: 10;"
+			class="absolute bg-black h-full w-full transition-transform"
+			class:rounded-xl={isSelected}
+			class:scale-[0.85]={isSelected}
 		/>
 		<div
-			style="position: absolute; width: {box.width}px; height: {box.height}px; top: {box.top}px; left: {box.left}px; z-index: 20;"
-		>
+			class="absolute z-10 h-full w-full bg-gradient-to-b from-black/25 via-[transparent_25%] opacity-0 transition-opacity group-hover:opacity-100"
+			class:rounded-xl={isSelected}
+			class:scale-[0.85]={isSelected}
+		/>
+		<div class="absolute z-20 h-full w-full">
 			{#if inSelectionMode || isMouseOver}
+				{@const icon = isSelected
+					? mdiCheckboxMarkedCircle
+					: inSelectionMode
+						? mdiCircleOutline
+						: mdiCheckCircleOutline}
 				<button
-					class="absolute p-2 focus:outline-none"
+					class="absolute m-1 p-1 focus:outline-none"
 					role="checkbox"
-					aria-checked="false"
+					aria-checked={isSelected}
 					onclick={(e) => {
 						e.stopPropagation();
+						e.preventDefault();
 						timeline.setAssetSelected(assetIndex, !isSelected);
 					}}
+					transition:fade={{ duration: 80 }}
 				>
 					<svg style="opacity: 0.6;" width="24" height="24" viewBox="0 0 24 24"
-						><path d={isSelected ? mdiCheckCircle : mdiCheckCircleOutline} fill="#fff" /></svg
+						><path d={icon} fill="#fff" /></svg
 					>
 				</button>
 			{/if}
 		</div>
 	</div>
-	<div
-		class="absolute z-10 h-full w-full bg-gradient-to-b from-black/25 via-[transparent_25%] opacity-0 transition-opacity group-hover:opacity-100"
-	></div>
 </a>

4143e3d198d77d46753b55e2d601956cb6b21941
Thomas Nibler <dev@tnibler.de>
2024-04-09 16:11:39 +0200

    always return packaging only + reencoding jobs
    previously if any shaka-packager only job was due only those would be returned,
    skipping the query for encoding+packaging jobs

--- server/core/src/catalog/rules.rs
+++ server/core/src/catalog/rules.rs
@@ -188,6 +188,7 @@ pub async fn thumbnails_to_create(conn: &mut PooledDbConn) -> Result<Vec<CreateT
         .collect())
 }
 
+#[tracing::instrument(skip(conn))]
 pub async fn video_packaging_due(conn: &mut PooledDbConn) -> Result<Vec<PackageVideo>> {
     // priority:
     //  - videos with original in acceptable codec and no DASH packaged
@@ -239,114 +240,92 @@ pub async fn video_packaging_due(conn: &mut PooledDbConn) -> Result<Vec<PackageV
             }
         }
     }
-    if !acceptable_codecs_no_dash_and_no_rotation_metadata.is_empty() {
-        // FIXME shaka into ffmpeg does not work!
-        // adding stream rotation tag with ffmpeg messes up offsets in mpd manifest and playback
-        // breaks.
-        // segmenting without reencoding only works if there is no stream rotation metadata tag,
-        // we'll need to reencode in that case
-        return Ok(acceptable_codecs_no_dash_and_no_rotation_metadata
-            .into_iter()
-            .map(|asset| {
-                // if there is no dash resource directory then there can not already be an audio
-                // representation, so create one if the video has audio
-                let create_audio_repr = if asset.video.audio_codec_name.is_some() {
-                    Some(CreateAudioRepr::PackageOriginalFile {
-                        output_key: storage_key::dash_file(
-                            asset.base.id,
-                            format_args!("audio.mp4"),
-                        ),
-                    })
-                } else {
-                    // video does not have audio
-                    None
-                };
-                // likewise for video representations
-                let existing_video_reprs = Vec::default();
-                let video_out_key = storage_key::dash_file(
-                    asset.base.id,
-                    format_args!("{}x{}.mp4", asset.base.size.width, asset.base.size.height),
-                );
-                PackageVideo {
-                    asset_id: asset.base.id,
-                    create_video_repr: CreateVideoRepr::PackageOriginalFile {
-                        output_key: video_out_key,
-                    },
-                    create_audio_repr,
-                    existing_video_reprs,
-                    mpd_out_key: storage_key::mpd_manifest(asset.base.id),
-                }
-            })
-            .collect());
-    }
+    let package_orig_tasks = acceptable_codecs_no_dash_and_no_rotation_metadata
+        .into_iter()
+        .map(|asset| {
+            // if there is no dash resource directory then there can not already be an audio
+            // representation, so create one if the video has audio
+            let create_audio_repr = if asset.video.audio_codec_name.is_some() {
+                Some(CreateAudioRepr::PackageOriginalFile {
+                    output_key: storage_key::dash_file(asset.base.id, format_args!("audio.mp4")),
+                })
+            } else {
+                // video does not have audio
+                None
+            };
+            // likewise for video representations
+            let existing_video_reprs = Vec::default();
+            let video_out_key = storage_key::dash_file(
+                asset.base.id,
+                format_args!("{}x{}.mp4", asset.base.size.width, asset.base.size.height),
+            );
+            PackageVideo {
+                asset_id: asset.base.id,
+                create_video_repr: CreateVideoRepr::PackageOriginalFile {
+                    output_key: video_out_key,
+                },
+                create_audio_repr,
+                existing_video_reprs,
+                mpd_out_key: storage_key::mpd_manifest(asset.base.id),
+            }
+        });
 
     let no_good_reprs: Vec<VideoAsset> = interact!(conn, move |mut conn| {
         repository::asset::get_video_assets_with_no_acceptable_repr(&mut conn)
     })
     .await??;
-    if !no_good_reprs.is_empty() || !must_reencode_because_rotation_metadata.is_empty() {
-        return Ok(no_good_reprs
-            .into_iter()
-            .chain(must_reencode_because_rotation_metadata.into_iter())
-            .map(|asset| {
-                let video_out_key = storage_key::dash_file(
-                    asset.base.id,
-                    format_args!("{}x{}.mp4", asset.base.size.width, asset.base.size.height),
-                );
-                let create_video_repr = match asset.video.video_codec_name.as_str() {
-                    // TODO replace with target codec from config and only transcode if the
-                    // original codec is unacceptable. Or maybe transcode anyway, provide a config
-                    // option etc..
-                    "av1" => CreateVideoRepr::PackageOriginalFile {
-                        output_key: video_out_key,
+    let reencode_tasks = no_good_reprs
+        .into_iter()
+        .chain(must_reencode_because_rotation_metadata.into_iter())
+        .map(|asset| {
+            let video_out_key = storage_key::dash_file(
+                asset.base.id,
+                format_args!("{}x{}.mp4", asset.base.size.width, asset.base.size.height),
+            );
+            let create_video_repr = match asset.video.video_codec_name.as_str() {
+                // TODO replace with target codec from config and only transcode if the
+                // original codec is unacceptable. Or maybe transcode anyway, provide a config
+                // option etc..
+                "av1" => CreateVideoRepr::PackageOriginalFile {
+                    output_key: video_out_key,
+                },
+                _ => CreateVideoRepr::Transcode(VideoTranscode {
+                    target: VideoEncodingTarget {
+                        codec: CodecTarget::AV1(av1::AV1Target {
+                            crf: av1::Crf::default(),
+                            fast_decode: None,
+                            preset: None,
+                            max_bitrate: None,
+                        }),
+                        scale: None,
                     },
-                    _ => CreateVideoRepr::Transcode(VideoTranscode {
-                        target: VideoEncodingTarget {
-                            codec: CodecTarget::AV1(av1::AV1Target {
-                                crf: av1::Crf::default(),
-                                fast_decode: None,
-                                preset: None,
-                                max_bitrate: None,
-                            }),
-                            scale: None,
-                        },
-                        output_key: video_out_key,
-                    }),
-                };
-                let audio_out_key =
-                    storage_key::dash_file(asset.base.id, format_args!("audio.mp4"));
-                // TODO actually check existing reprs in database.
-                // maybe the acceptable video in config changed, making us reencode video
-                // but actually a suitable audio repr already exists (or vice versa)
-                let create_audio_repr = match asset.video.audio_codec_name.as_deref() {
-                    Some("aac" | "opus" | "mp3") => Some(CreateAudioRepr::PackageOriginalFile {
-                        output_key: audio_out_key,
-                    }),
-                    // TODO matching strings is ehh since we only allow a few codecs anyway
-                    Some(_) => Some(CreateAudioRepr::Transcode(AudioTranscode {
-                        target: AudioEncodingTarget::OPUS,
-                        output_key: audio_out_key,
-                    })),
-                    None => None,
-                };
-                PackageVideo {
-                    asset_id: asset.base.id,
-                    create_video_repr,
-                    create_audio_repr,
-                    existing_video_reprs: Vec::default(),
-                    mpd_out_key: storage_key::mpd_manifest(asset.base.id),
-                }
-            })
-            .collect());
-    }
-    // return Ok(vec![]);
-    // if all videos have at least one good repr:
-    //   for every rung in quality_levels starting from the highest
-    //     for every video, ql = quality_ladder(video):
-    //       if rung in ql and no repr for video at that rung:
-    //         transcode to that rung
-    // TODO
-    Ok(Vec::default())
+                    output_key: video_out_key,
+                }),
+            };
+            let audio_out_key = storage_key::dash_file(asset.base.id, format_args!("audio.mp4"));
+            // TODO actually check existing reprs in database.
+            // maybe the acceptable video in config changed, making us reencode video
+            // but actually a suitable audio repr already exists (or vice versa)
+            let create_audio_repr = match asset.video.audio_codec_name.as_deref() {
+                Some("aac" | "opus" | "mp3") => Some(CreateAudioRepr::PackageOriginalFile {
+                    output_key: audio_out_key,
+                }),
+                // TODO matching strings is ehh since we only allow a few codecs anyway
+                Some(_) => Some(CreateAudioRepr::Transcode(AudioTranscode {
+                    target: AudioEncodingTarget::OPUS,
+                    output_key: audio_out_key,
+                })),
+                None => None,
+            };
+            PackageVideo {
+                asset_id: asset.base.id,
+                create_video_repr,
+                create_audio_repr,
+                existing_video_reprs: Vec::default(),
+                mpd_out_key: storage_key::mpd_manifest(asset.base.id),
+            }
+        });
+    Ok(package_orig_tasks.chain(reencode_tasks).collect())
 }
 
 pub async fn image_conversion_due(conn: &mut PooledDbConn) -> Result<Vec<ConvertImage>> {

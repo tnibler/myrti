9a73132d4c6b4f63a014011305401377e554f99d
Thomas Nibler <dev@tnibler.de>
2024-07-22 20:36:39 +0200

    web: enter/escape keybinds in create group title input

--- web/src/lib/timeline-grid/CreateGroupInput.svelte
+++ web/src/lib/timeline-grid/CreateGroupInput.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Button from '@lib/ui/Button.svelte';
   import type { TimelineGridItem } from './timeline.svelte';
+  import type { ActionReturn } from 'svelte/action';
+  import { onMount } from 'svelte';
 
   type Props = {
     item: TimelineGridItem & { type: 'createGroupTitleInput' };
@@ -11,20 +13,42 @@
   const { item, onSubmit, onCancel }: Props = $props();
   let input: HTMLInputElement | null = null;
 
-  // TODO: enter/escape to confirm/cancel create title field
+  onMount(() => {
+    input?.focus();
+  });
+
+  function trySubmit() {
+    const title = input?.value?.trim();
+    if (title !== undefined && title.length > 0) {
+      onSubmit(title);
+    }
+  }
+
+  function inputKeyBinds(el: HTMLInputElement): ActionReturn {
+    el.onkeyup = (e) => {
+      if (e.key === 'Escape') {
+        e.preventDefault();
+        onCancel();
+      } else if (e.key === 'Enter') {
+        trySubmit();
+      }
+    };
+    return {
+      destroy: () => {
+        el.onkeyup = null;
+      },
+    };
+  }
+
   // TODO: disable button if title input is empty
 </script>
 
-<div class="absolute flex-row" style="top: {item.top}px; left: {item.left}px;">
-  <input placeholder="Enter group title" bind:this={input} />
+<div class="absolute w-full flex-row" style="top: {item.top}px;">
+  <input use:inputKeyBinds placeholder="Enter group title" bind:this={input} />
   <Button
     text="Ok"
     onclick={() => {
-      const title = input?.value;
-      if (title === undefined) {
-        return;
-      }
-      onSubmit(title);
+      trySubmit();
     }}
   />
   <Button

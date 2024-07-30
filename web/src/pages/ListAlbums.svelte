ca9fda6d68b2e1bd90553a8b6bcf1efe6fd6baf7
Thomas Nibler <dev@tnibler.de>
2024-07-30 07:50:45 +0200

    web: display album thumbnail in ListAlbums view

--- web/src/pages/ListAlbums.svelte
+++ web/src/pages/ListAlbums.svelte
@@ -23,6 +23,7 @@
 				<img
 					class="flex-1 w-full aspect-square rounded-xl bg-gray-500"
 					alt={album.name}
+					src="/api/albums/{album.id}/thumbnail/SIZE_UNUSED/avif"
 				/>
 				<p class="ml-1 mt-1 font-medium">{album.name}</p>
 				<p class="ml-1 font-medium text-xs">{album.numAssets} element</p>

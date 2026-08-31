53cd983ea00b7822a8e5bfdba67c843339dea60a
Thomas Nibler <dev@tnibler.de>
2026-08-31 12:24:59 +0200

    make optional props optional

--- web/src/lib/ui/Button.svelte
+++ web/src/lib/ui/Button.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import type { MouseEventHandler } from 'svelte/elements';
+  import type { MouseEventHandler } from 'svelte/elements';
 
-	type Props = {
-		text: string;
-		onclick: MouseEventHandler<HTMLButtonElement> | undefined;
-		primary: boolean | undefined;
-		class: string | undefined;
-	};
-	const { text, onclick, primary = undefined, ...otherProps }: Props = $props();
+  type Props = {
+    text: string;
+    onclick: MouseEventHandler<HTMLButtonElement> | undefined;
+    primary?: boolean | undefined;
+    class?: string | undefined;
+  };
+  const { text, onclick, primary = undefined, ...otherProps }: Props = $props();
 </script>
 
 <button
-	class="w-fit py-2 px-3 rounded-md transition-all font-medium text-md {primary
-		? 'bg-purple-300 hover:shadow-md'
-		: 'bg-transparent hover:bg-gray-200'} {otherProps['class']}"
-	{onclick}
+  class="w-fit py-2 px-3 rounded-md transition-all font-medium text-md {primary
+    ? 'bg-purple-300 hover:shadow-md'
+    : 'bg-transparent hover:bg-gray-200'} {otherProps['class']}"
+  {onclick}
 >
-	{text}
+  {text}
 </button>

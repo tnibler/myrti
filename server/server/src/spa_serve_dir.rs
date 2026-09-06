ef41d6c95d13663545f34da79162e705cea413b4
Thomas Nibler <dev@tnibler.de>
2026-09-07 00:22:10 +0200

    fix url rewriting

--- server/server/src/spa_serve_dir.rs
+++ server/server/src/spa_serve_dir.rs
@@ -29,17 +29,51 @@ where
     }
 
     fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
-        if req.uri().path().starts_with("/albums") {
-            let mut b = Uri::builder();
-            if let Some(scheme) = req.uri().scheme() {
-                b = b.scheme(scheme.clone());
-            }
-            if let Some(authority) = req.uri().authority() {
-                b = b.authority(authority.clone());
-            }
-            b = b.path_and_query("albums.html");
-            *req.uri_mut() = b.build().expect("url is copied from request");
+        if let Some(rewritten) = rewrite_uri(req.uri()) {
+            *req.uri_mut() = rewritten;
         }
         self.serve_dir.call(req)
     }
+}
+
+fn rewrite_uri(in_uri: &Uri) -> Option<Uri> {
+    if in_uri.path().starts_with("/albums") {
+        let mut b = Uri::builder();
+        if let Some(scheme) = in_uri.scheme() {
+            b = b.scheme(scheme.clone());
+        }
+        if let Some(authority) = in_uri.authority() {
+            b = b.authority(authority.clone());
+        }
+        b = b.path_and_query("/albums.html");
+        Some(b.build().expect("url must be valid"))
+    } else {
+        None
+    }
+}
+
+#[test]
+fn url_rewriting_works() {
+    assert_eq!(None, rewrite_uri(&Uri::from_static("https://example.org")));
+    assert_eq!(None, rewrite_uri(&Uri::from_static("https://example.org/")));
+    assert_eq!(
+        None,
+        rewrite_uri(&Uri::from_static("http://localhost:5173/map"))
+    );
+    assert_eq!(
+        Some(Uri::from_static("http://localhost/albums.html")),
+        rewrite_uri(&Uri::from_static("http://localhost/albums/1"))
+    );
+    assert_eq!(
+        Some(Uri::from_static("http://localhost:5173/albums.html")),
+        rewrite_uri(&Uri::from_static("http://localhost:5173/albums"))
+    );
+    assert_eq!(
+        Some(Uri::from_static("http://subdomain.example.org/albums.html")),
+        rewrite_uri(&Uri::from_static("http://subdomain.example.org/albums"))
+    );
+    assert_eq!(
+        None,
+        rewrite_uri(&Uri::from_static("https://example.org/timeline/1234"))
+    );
 }
